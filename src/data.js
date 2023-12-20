4ee42b7290ba2a2dacbcec0f07d0dc05dceaf6e6
Snehal's Windows10 <snehalgajbhiye833@gmail.com>
2023-12-21 00:29:57 +0530

    updated files

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export let moviesList = ['Animal', 'Samबहादुर', '12th Fail', 'Tiger 3', 'Napoleon', "Salaar"];
+export let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM", "12:00 PM"];
+export let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+
+// exports.moviesList = moviesList;
+// exports.slots = slots;
+// exports.seats = seats;
